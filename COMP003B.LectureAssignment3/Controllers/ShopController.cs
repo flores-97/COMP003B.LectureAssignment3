1d735429f7327d0e06202769ea37ed6007ab4be3
flores-97 <floresvictor15@gmail.com>
2025-03-31 10:03:28 -0700

    shop controller replicated

---
+++ COMP003B.LectureAssignment3/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.LectureAssignment3.Controllers
+{
+    public class ShopController : Controller
+    {
+        //GET: /shop/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //view methods return a view of results that rendersa  view of the respones
+            return View();
+        }
+
+        //GET: /shop/details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            //the view method returns a view that renders a view to the response
+            //the id parament is passed to the view
+            return View(id);
+        }
+    }
+}
